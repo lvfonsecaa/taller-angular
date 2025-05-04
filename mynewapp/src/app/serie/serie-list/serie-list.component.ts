560706103471247307c358e4ca1d26965e68efb8
lvfonsecaa <lv.fonseca@uniandes.edu.co>
2025-05-04 18:15:12 -0500

    aksjdnaskjd

--- mynewapp/src/app/serie/serie-list/serie-list.component.ts
+++ mynewapp/src/app/serie/serie-list/serie-list.component.ts
@@ -26,4 +26,22 @@ export class SerieListComponent implements OnInit {
     this.getSeries();
   }
 
+  selectedSerie: Serie | null = null;
+
+  selectSerie(serie: Serie) {
+    this.selectedSerie = serie;
+  }
+
+  promedio (serie:Serie):number
+  {
+    let suma = 0;
+    let numeroSeries = 0;
+    for (let i = 0; i < this.series.length; i++) {
+        suma += this.series[i].seasons;
+        numeroSeries++;
+    }
+    return suma/numeroSeries;
+
+  }
+
 }
